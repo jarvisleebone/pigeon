e3208d025a3cb469e2c493eb1a02c411ce79ad03
李想 <lixiang@zentech-inc.com>
2017-09-15 17:35:12 +0800

    commit

--- pigeon-core/src/main/java/org/pigeon/proxy/ClientInvocationHandler.java
+++ pigeon-core/src/main/java/org/pigeon/proxy/ClientInvocationHandler.java
@@ -33,6 +33,9 @@ public class ClientInvocationHandler<T> implements InvocationHandler {
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        // TODO 本地方法调用
+
+
         // 封装请求
         PigeonRequest request = new PigeonRequest();
         request.setInterfaceName(clazz.getName());
